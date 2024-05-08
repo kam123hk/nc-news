45a0e063fdbd1cae638de827b3d5241921cebc6d
Kam Cheng <kam.cheng03@gmail.com>
2024-05-08 16:42:35 +0100

    complete task 7- patch article with increment or decrement vote with optimistic rendering and error alert

--- src/api.js
+++ src/api.js
@@ -30,3 +30,32 @@ export async function fetchComments(article_id, setCommentsArray, setIsLoading) 
         // handle error
     }
 }
+
+// fetch("https://nc-marketplace-sem-2.onrender.com/api/items", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(postedItem),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         return showNewItem(data.item);
+//       });
+
+export async function patchVote(article_id, voteChange) {
+    try {
+        const response = await fetch(`https://first-nc-project.onrender.com/api/articles/${article_id}`,
+            {
+                method: "PATCH",
+                headers: {
+                            "Content-Type": "application/json",
+                          },
+                body: JSON.stringify({inc_votes: voteChange})
+            }
+        )
+        await response.json();
+    } catch (error) {
+        alert(error.message);
+    }
+}
