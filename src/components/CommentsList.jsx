e92ba1be13fa45b26fa4361eff377533cfc055f9
Kam Cheng <kam.cheng03@gmail.com>
2024-05-09 14:50:59 +0100

    complete task 8- Post comment to article with comment input in comment list component

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -2,20 +2,24 @@ import { useState, useEffect } from "react";
 import CommentCard from "./CommentCard";
 import { fetchComments } from "../api";
 import Loading from "./Loading";
+import CommentInput from "./CommentInput";
 
-function CommentsList({ article_id }) {
+function CommentsList({ article_id, updateCommentCount }) {
 
     const [commentsArray, setCommentsArray] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetchComments(article_id, setCommentsArray, setIsLoading);
-    }, [])
+    }, [commentsArray])
 
     if (isLoading) return <Loading />
     else {
         return (
-        <CommentCard commentsArray={commentsArray} />
+        <>
+        <CommentInput article_id={article_id} updateCommentCount={updateCommentCount}/>
+        <CommentCard commentsArray={commentsArray} />            
+        </>        
         )
     }
 }
